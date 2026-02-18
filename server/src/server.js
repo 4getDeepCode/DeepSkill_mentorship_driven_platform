78e7ca1072a3b71361776aabe61c44f8bfa1b075
Deep Chand <deepc2060@gmail.com>
2026-02-18 21:05:03 +0530

    add frontend url

--- server/src/server.js
+++ server/src/server.js
@@ -10,7 +10,12 @@ const errorHandler = require("./middleware/error");
 const routes = require("./routes/v1");
 const config = require("./config");
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "deep-skill-mentorship-driven-platfo-ashy.vercel.app",
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
